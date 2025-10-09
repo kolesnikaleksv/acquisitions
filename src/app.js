2b54592de1a678f4a35a725d81e3ba9fe8e8577f
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-10-09 18:38:45 +0300

    Change import for authRoutes in app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-// import authRoutes from '#routes/auth.routes.js';
-import authRoutes from './routes/auth.routes.js';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
